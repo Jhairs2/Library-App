06d4491cd63bfe11e89cd693f208b9da9bac3428
Justin Hairston <jhairst2@vols.utk.edu>
2022-09-28 21:46:37 -0400

    Refactor code

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,22 @@
 // // Global variables
 let myLibrary = []
 
-const openModal = document.getElementById("addBook");
 const modal = document.getElementById("modal");
+const openModal = document.getElementById("addBook");
 const closeModal = document.getElementById("close-modal");
 const submit = document.querySelector(".add");
 
 
-// 
-
-// Event listeners to open and close modal
-openModal.addEventListener("click", () => {
-    modal.showModal();
-    modal.style.display = "";
-})
-
-closeModal.addEventListener("click", () => {
-    modal.style.display = "none";
-    modal.close();
-})
+// Event listeners 
+submit.addEventListener("click", submitForm);
+openModal.addEventListener("click", openMod)
+closeModal.addEventListener("click", closeMod)
 
 
 
+// Functions
 
+// Create book constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -32,6 +26,7 @@ function Book(title, author, pages, read) {
 
 };
 
+// Give toggle function to book object
 Book.prototype.readToggle = function readToggle(toggle) {
     if (toggle.checked) {
         return this.read = "Read";
@@ -40,65 +35,34 @@ Book.prototype.readToggle = function readToggle(toggle) {
     }
 }
 
-
+// Will add book to library array
 function addBooksToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
     showBooks();
 }
 
+// Function opens modal
+function openMod() {
+    modal.showModal();
+    modal.style.display = "";
+}
+// Function closes modal
+function closeMod() {
+    modal.style.display = "none";
+    modal.close();
+}
 
-function showBooks() {
-    const mainContainer = document.querySelector(".main-content");
-    const removeCards = document.querySelectorAll(".card");
-
-
-    for (let i = 0; i < removeCards.length; i++) {
-        removeCards[i].remove();
+// Remove all cards on screen before add new card
+function removeCards() {
+    const cards = document.querySelectorAll(".card");
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].remove();
     };
+}
 
-
-    let index = 0;
-    let readIndex = 0;
-    // Append book card with book info to content
-    myLibrary.forEach(myLibrary => {
-        const card = document.createElement("div");
-        const readContainer = document.createElement("div");
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("delete");
-        card.classList.add("card");
-        deleteButton.dataset.index = index;
-        readContainer.classList.add("read-check");
-        mainContainer.appendChild(card);
-        index++;
-        for (let key in myLibrary) {
-
-            if (key !== "read" & key !== "readToggle") {
-                const p = document.createElement("p");
-                p.textContent = (`${key}: ${myLibrary[key]}`);
-                card.appendChild(p);
-            }
-            else if (key == "read") {
-
-                const readCheck = document.createElement("input")
-                const p = document.createElement("p");
-                readCheck.type = "checkbox";
-                readCheck.classList.add("readToggle");
-                readCheck.dataset.index = readIndex;
-                readIndex++;
-                p.textContent = (`${key}: ${myLibrary[key]}`);
-                p.classList.add("tog");
-                deleteButton.textContent = "Remove";
-                readContainer.appendChild(p);
-                readContainer.appendChild(readCheck);
-                card.appendChild(readContainer);
-                card.appendChild(deleteButton);
-
-            }
-        }
-
-    });
-
+// Function will allow user to delete a card from library at certain index
+function deleteCard() {
     const deleteBttn = document.querySelectorAll(".delete");
     deleteBttn.forEach(button => {
         button.addEventListener("click", () => {
@@ -107,10 +71,14 @@ function showBooks() {
             showBooks();
         })
     })
+}
 
+// Function allows user to toggle read button
+function toggleRead() {
+    const toggleRead = document.querySelectorAll(".read-toggle");
+    const messageToggle = document.querySelectorAll(".read-message");
 
-    const toggleRead = document.querySelectorAll(".readToggle");
-    const togs = document.querySelectorAll(".tog");
+    // Set checkbox to be checked if Read and unchecked if not
     toggleRead.forEach(toggle => {
         let index = toggle.dataset.index;
         if (myLibrary[index].read == "Read") {
@@ -119,21 +87,89 @@ function showBooks() {
         else {
             toggle.checked = false;
         }
+
+        // Add event listener to checkbox to change the read status 
         toggle.addEventListener("change", (e) => {
+
+            // readCheck will inherit toggle function from Book prototype to change book object status
             const readCheck = Object.create(Book.prototype);
-            togs[index].textContent = `Read: ${readCheck.readToggle(e.target)}`;
+            // Will change status at index of card selected
+            messageToggle[index].textContent = `Read: ${readCheck.readToggle(e.target)}`;
         })
     })
 }
 
+// Function will create library card with user information and show it on screen to user
+function showBooks() {
+    const mainContainer = document.querySelector(".main-content");
+    // Remove all cards
+    removeCards();
+
+    // Keep track of index for delete and read toggle buttons
+    let index = 0;
+    let readIndex = 0;
+
+    // Append book card with book info to content
+    myLibrary.forEach(myLibrary => {
+
+        // Create book card elements
+        const card = document.createElement("div");
+        const readContainer = document.createElement("div");
+        const deleteButton = document.createElement("button"); deleteButton.textContent = "Remove";
+
+        // Add classes and data attributes
+        deleteButton.classList.add("delete");
+        card.classList.add("card");
+        readContainer.classList.add("read-check");
+        deleteButton.dataset.index = index;
+
+        // Append card to main content and increase index var for next card
+        mainContainer.appendChild(card);
+        index++;
+
+        // Add book info to card
+        for (let key in myLibrary) {
+
+            // Add book title, author, and pages info
+            if (key !== "read" & key !== "readToggle") {
+                const p = document.createElement("p");
+                p.textContent = (`${key}: ${myLibrary[key]}`);
+                card.appendChild(p);
+            }
+            else if (key == "read") {
+
+                // create read check variables
+                const readCheck = document.createElement("input"); readCheck.type = "checkbox";
+                const p = document.createElement("p");
+
+                // Add classes and data attributes
+                readCheck.classList.add("read-toggle");
+                p.classList.add("read-message");
+                readCheck.dataset.index = readIndex;
+
+                // Add info to variables
+                p.textContent = (`${key}: ${myLibrary[key]}`);
+
+                // Append container with elements to card and increase index var for next card
+                readContainer.appendChild(p);
+                readContainer.appendChild(readCheck);
+                card.appendChild(readContainer);
+                card.appendChild(deleteButton);
+                readIndex++;
+            }
+        }
+
+    });
+
+    deleteCard();
+    toggleRead();
+
+}
 
 
+// Submit Form with user information
+function submitForm(e) {
 
-
-
-
-
-submit.addEventListener("click", (e) => {
     e.preventDefault();
     const readCheck = Object.create(Book.prototype);
     const formInputs = document.querySelectorAll("input");
@@ -142,15 +178,16 @@ submit.addEventListener("click", (e) => {
     author = formInputs[1].value;
     pages = formInputs[2].value;
 
+    // Will not submit anything if form is empty
     if (title == "" || author == "" || pages == "") {
         return;
-      }
+    }
     addBooksToLibrary(title, author, pages, readCheck.readToggle(formInputs[3]));
     form.reset();
-    modal.style.display = "none";
-    modal.close();
-})
+    closeMod();
+}
 
 
 
-addBooksToLibrary("Blow", "Kim Gourd", "521", "Read");
+
+addBooksToLibrary("It Ends with Us: A Novel", "Colleen Hoover", "381", "Not Read");
